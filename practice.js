51c7291845d4b1fbf121ef9fb8a4fc882b1174e6
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-10 15:18:12 -0700

    migrate proj from vanilla node to express, added gitignore

--- practice.js
+++ practice.js
@@ -1,52 +1,80 @@
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.setHeader('Content-Type', 'text/html')
+const app = express();
 
-  path = '';
-  switch (req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
-      break;
-
-    case '/about':
-      path += 'about.html';
-      res.statusCode = 200;
-      break;
-    
-    case '/contact-me':
-      path += 'contact-me.html';
-      res.statusCode = 200;
-      break;
-
-    case '/contact':
-      res.statusCode = 301;
-      res.setHeader('Location', '/contact-me')
-      res.end();
-      break;
-    
-    default:
-      path += '404.html';
-      res.statusCode = 404
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err){
-      console.log(err)
-      res.end()
-    } else {
-      res.write(data, () => {
-        console.log("write completed")
-      })
-      res.end()
-    }
-  })
+// route configuration
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
 })
 
-server.listen('3000', 'localhost', () => {
+app.get('/about', (req, res) => {
+  res.sendFile('about.html', { root: __dirname });
+})
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile('contact-me.html', { root: __dirname });
+})
+
+app.get('/contact', (req, res) => {
+  res.redirect('/contact-me');
+})
+
+app.use((req, res) => {
+  res.status(404).sendFile('404.html', { root: __dirname })
+})
+
+// starting server
+app.listen('3000', () => {
   console.log('listening for requests')
 })
+
+
+/* OLD CODE */ 
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   res.setHeader('Content-Type', 'text/html')
+
+//   path = '';
+//   switch (req.url) {
+//     case '/':
+//       path += 'index.html';
+//       res.statusCode = 200;
+//       break;
+
+//     case '/about':
+//       path += 'about.html';
+//       res.statusCode = 200;
+//       break;
+    
+//     case '/contact-me':
+//       path += 'contact-me.html';
+//       res.statusCode = 200;
+//       break;
+
+//     case '/contact':
+//       res.statusCode = 301;
+//       res.setHeader('Location', '/contact-me')
+//       res.end();
+//       break;
+    
+//     default:
+//       path += '404.html';
+//       res.statusCode = 404
+//       break;
+//   }
+
+//   fs.readFile(path, (err, data) => {
+//     if (err){
+//       console.log(err)
+//       res.end()
+//     } else {
+//       res.write(data, () => {
+//         console.log("write completed")
+//       })
+//       res.end()
+//     }
+//   })
+// })
