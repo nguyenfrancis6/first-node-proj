7c2619a153017be08d0ec05da0eefc5f4ab718b3
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-09-10 14:46:31 -0700

    First project using node

---
+++ practice.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.setHeader('Content-Type', 'text/html')
+
+  path = '';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    
+    case '/contact-me':
+      path += 'contact-me.html';
+      res.statusCode = 200;
+      break;
+
+    case '/contact':
+      res.statusCode = 301;
+      res.setHeader('Location', '/contact-me')
+      res.end();
+      break;
+    
+    default:
+      path += '404.html';
+      res.statusCode = 404
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err){
+      console.log(err)
+      res.end()
+    } else {
+      res.write(data, () => {
+        console.log("write completed")
+      })
+      res.end()
+    }
+  })
+})
+
+server.listen('3000', 'localhost', () => {
+  console.log('listening for requests')
+})
